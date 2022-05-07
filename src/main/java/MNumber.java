493e6a5ffb7e802cf46ed5fa33ab195a2e24d207
SafronovaOlga <olgasafronova.636531448@gmail.com>
2022-05-07 19:20:00 +0200

    added a new class MNumber

--- src/main/java/MNumber.java
+++ src/main/java/MNumber.java
@@ -1,2 +1,19 @@
 public class MNumber {
+
+    public String getMNumbers (int m) {
+
+        if (m % 3 == 0 && m % 5 == 0) {
+
+            return "Good Number";
+        } else if (m % 3 == 0 && m % 5 != 0) {
+
+            return "Bad Number";
+        } else if (m % 5 == 0 && m % 3 != 0) {
+
+            return "Poor Number";
+        }else {
+
+            return "-1";
+        }
+    }
 }
--- src/test/java/MNumberTest.java
+++ src/test/java/MNumberTest.java
@@ -1,2 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class MNumberTest {
+
+    @Test
+    public void testMNumberGoodNumber() {
+
+        int m = 15;
+        String expectedResult = "Good Number";
+
+        MNumber mNumber = new MNumber();
+        String actualResult = mNumber.getMNumbers(m);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
