cd41aab8d94eb3e417ab4dac7d6e584beac10466
SafronovaOlga <olgasafronova.636531448@gmail.com>
2022-05-24 20:07:45 +0200

    Changed, added, created a class

---
+++ src/test/java/HW_12.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class HW_12 {
+
+    @Test
+    public void testMenuTopList (){
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/olgasafronova/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "Top Lists";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement searchTopList = driver.findElement(
+                By.xpath("//div[@id='navigation']/ul[@id= 'menu']//a[@href='/toplist.html']"));
+
+
+        String actualResult = searchTopList.getText();
+
+        Assert.assertEquals(actualResult, expectedResult.toUpperCase());
+
+        driver.quit();
+    }
+}
